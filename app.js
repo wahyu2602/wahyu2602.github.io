d3876b538b9c7a7b93cf96aea57e64e933ffea31
Wahyu <65730786+wahyu2602@users.noreply.github.com>
2021-10-27 16:07:20 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+import "./vendor/bootstrap/dist/js/bootstrap.bundle.js";
+import "./vendor/jquery/dist/jquery.js";
+import main from "./src/script/script.js";
+window.addEventListener("DOMContentLoaded", main);
