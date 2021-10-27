90382b9286f8b4614b8c2ad38b7f1223904b262c
Wahyu <65730786+wahyu2602@users.noreply.github.com>
2021-10-27 16:38:28 +0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
-import "./vendor/bootstrap/dist/js/bootstrap.bundle.js";
-import "./vendor/jquery/dist/jquery.js";
 import main from "./src/script/script.js";
 window.addEventListener("DOMContentLoaded", main);
